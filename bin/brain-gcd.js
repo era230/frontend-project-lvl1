3a33ad05bc09c006d5430f42285154dcc3664a8e
Elvira Aitkulova <aitkulovael@gmail.com>
2022-05-16 19:01:41 +0300

    add new game GCD

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import checkGcd from '../games/gcd.js';
+
+checkGcd();
---
+++ games/gcd.js
@@ -0,0 +1,34 @@
+import bodyOfTheGame from '../src/index.js';
+
+const getGcd = (a, b) => {
+  let num1 = Math.max(a, b);
+  let num2 = Math.min(a, b);
+  let reminder = num1 % num2;
+  while (num1 % num2 !== 0) {
+    reminder = num1 % num2;
+    num1 = num2;
+    num2 = reminder;
+  }
+  return num2;
+};
+
+const makeArr = () => {
+  const resultArr = [];
+  for (let i = 1; i <= 3; i += 1) {
+    const randomNumber1 = Math.floor(Math.random() * 100);
+    const randomNumber2 = Math.floor(Math.random() * 100);
+    const expression = String(`${randomNumber1} ${randomNumber2}`);
+    resultArr.push(expression);
+    const correctAnswer = String(getGcd(randomNumber1, randomNumber2));
+    resultArr.push(correctAnswer);
+  }
+  return resultArr;
+};
+
+const ruleOfTheGame = 'Find the greatest common divisor of given numbers.';
+
+const checkGcd = () => {
+  bodyOfTheGame(ruleOfTheGame, makeArr());
+};
+
+export default checkGcd;
