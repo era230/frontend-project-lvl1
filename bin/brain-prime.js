6aaf6d7741ce4533e8a6518f31c685479bf45ca1
Elvira Aitkulova <aitkulovael@gmail.com>
2022-05-17 19:44:05 +0300

    add brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import checkPrime from '../games/prime.js';
+
+checkPrime();
---
+++ games/prime.js
@@ -0,0 +1,29 @@
+import bodyOfTheGame from '../src/index.js';
+
+const prime = (num) => {
+  for (let i = 2; i * i < num; i += 1) {
+    if (num % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+
+const makeArr = () => {
+  const resultArr = [];
+  for (let i = 1; i <= 3; i += 1) {
+    const randomNumber = Math.floor(Math.random() * 200);
+    resultArr.push(randomNumber);
+    const correct = prime(randomNumber);
+    resultArr.push(correct);
+  }
+  return resultArr;
+};
+
+const ruleOfTheGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const checkPrime = () => {
+  bodyOfTheGame(ruleOfTheGame, makeArr());
+};
+
+export default checkPrime;
