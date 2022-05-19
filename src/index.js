23f392322905868fc0ffe690be01f8eb17bfc283
Elvira Aitkulova <aitkulovael@gmail.com>
2022-05-19 18:47:13 +0300

    fix quotes in wrong answer

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const runEngine = (ruleOfTheGame, gameValues) => {
     console.log(`Question: ${expression}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== correctAnswer) {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
